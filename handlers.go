d6bedcf25b8d10d5bc2270301b55bd68c7ac9908
hophiphip <nighop@yandex.ru>
2022-01-21 00:28:17 +0400

    Convert image.Image to bytes

--- handlers.go
+++ handlers.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/hophiphip/tilez/models"
 	"image"
-	"io/ioutil"
+	"image/png"
+	_ "image/png"
 	"net/http"
 	"os"
 )
 
 func handleImage(ctx *gin.Context) {
-	img := ctx.Param("img")
-	x := ctx.Param("x")
-	y := ctx.Param("y")
-	zoom := ctx.Param("zoom")
 
 	// parse args
-	parsedImage, err := models.New(img, x, y, zoom)
+	parsedImage, err := models.New(
+		ctx.Param("image"),
+		ctx.Param("x"),
+		ctx.Param("y"),
+		ctx.Param("zoom"))
+
 	if err != nil {
 		ctx.String(http.StatusBadRequest, fmt.Sprint(err))
 		return
@@ -25,29 +28,60 @@ func handleImage(ctx *gin.Context) {
 
 	reader, err := os.Open(parsedImage.ImagePath)
 	if err != nil {
-		ctx.String(http.StatusInternalServerError, fmt.Sprint(err))
+		ctx.String(http.StatusInternalServerError, fmt.Sprintf("os.Open: %s", err))
 		return
 	}
 
 	defer func() {
 		if err = reader.Close(); err != nil {
-			ctx.String(http.StatusInternalServerError, fmt.Sprint(err))
+			ctx.String(http.StatusInternalServerError, fmt.Sprintf("reader.Close: %s", err))
 			return
 		}
 	}()
 
-	imageType, _, err := image.Decode(reader)
+	img, _, err := image.Decode(reader)
 	if err != nil {
-		ctx.String(http.StatusInternalServerError, fmt.Sprint(err))
+		ctx.String(http.StatusInternalServerError, fmt.Sprintf("image.Decode: %s", err))
 		return
 	}
 
-	// read an img
-	imageBytes, err := ioutil.ReadFile(parsedImage.ImagePath)
-	if err != nil {
-		ctx.String(http.StatusInternalServerError, fmt.Sprint(err))
+	//bounds := img.Bounds()
+	//var newBounds image.Rectangle
+
+	imgBuffer := new(bytes.Buffer)
+
+	if err = png.Encode(imgBuffer, img); err != nil {
+		ctx.String(http.StatusInternalServerError, fmt.Sprintf("png.Encode: %s", err))
 		return
 	}
 
-	ctx.Data(http.StatusOK, "img/png", imageBytes)
+	ctx.Data(http.StatusOK, "image/png", imgBuffer.Bytes())
 }
+
+//reader, err := os.Open(parsedImage.ImagePath)
+//if err != nil {
+//ctx.String(http.StatusInternalServerError, fmt.Sprint(err))
+//return
+//}
+//
+//defer func() {
+//	if err = reader.Close(); err != nil {
+//		ctx.String(http.StatusInternalServerError, fmt.Sprint(err))
+//		return
+//	}
+//}()
+//
+//imageType, _, err := image.Decode(reader)
+//if err != nil {
+//ctx.String(http.StatusInternalServerError, fmt.Sprint(err))
+//return
+//}
+//
+//// read an img
+//imageBytes, err := ioutil.ReadFile(parsedImage.ImagePath)
+//if err != nil {
+//ctx.String(http.StatusInternalServerError, fmt.Sprint(err))
+//return
+//}
+//
+//ctx.Data(http.StatusOK, "img/png", imageBytes)
