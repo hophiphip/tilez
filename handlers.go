a50b35d7befc0c031e46c4aaba4e40c4a0d82796
hophiphip <nighop@yandex.ru>
2022-01-21 00:56:20 +0400

    Added image scaling

--- handlers.go
+++ handlers.go
@@ -45,12 +45,29 @@ func handleImage(ctx *gin.Context) {
 		return
 	}
 
-	//bounds := img.Bounds()
-	//var newBounds image.Rectangle
+	bounds := img.Bounds()
+
+	// NOTE: For now we suppose the image is square
+	newSize := (bounds.Max.X - bounds.Min.X) / parsedImage.Zoom
+
+	newBounds := image.Rectangle{
+		Min: image.Point{
+			X: newSize * parsedImage.X,
+			Y: newSize * parsedImage.Y,
+		},
+		Max: image.Point{
+			X: newSize * (parsedImage.X + 1),
+			Y: newSize * (parsedImage.Y + 1),
+		},
+	}
+
+	newImage := img.(interface {
+		SubImage(r image.Rectangle) image.Image
+	}).SubImage(image.Rect(newBounds.Min.X, newBounds.Min.Y, newBounds.Max.X, newBounds.Max.Y))
 
 	imgBuffer := new(bytes.Buffer)
 
-	if err = png.Encode(imgBuffer, img); err != nil {
+	if err = png.Encode(imgBuffer, newImage); err != nil {
 		ctx.String(http.StatusInternalServerError, fmt.Sprintf("png.Encode: %s", err))
 		return
 	}
@@ -58,30 +75,18 @@ func handleImage(ctx *gin.Context) {
 	ctx.Data(http.StatusOK, "image/png", imgBuffer.Bytes())
 }
 
-//reader, err := os.Open(parsedImage.ImagePath)
-//if err != nil {
-//ctx.String(http.StatusInternalServerError, fmt.Sprint(err))
-//return
-//}
-//
-//defer func() {
-//	if err = reader.Close(); err != nil {
-//		ctx.String(http.StatusInternalServerError, fmt.Sprint(err))
-//		return
-//	}
-//}()
-//
-//imageType, _, err := image.Decode(reader)
-//if err != nil {
-//ctx.String(http.StatusInternalServerError, fmt.Sprint(err))
-//return
-//}
-//
-//// read an img
-//imageBytes, err := ioutil.ReadFile(parsedImage.ImagePath)
-//if err != nil {
-//ctx.String(http.StatusInternalServerError, fmt.Sprint(err))
-//return
-//}
-//
-//ctx.Data(http.StatusOK, "img/png", imageBytes)
+func cropImage(img image.Image, crop image.Rectangle) (image.Image, error) {
+	type subImager interface {
+		SubImage(r image.Rectangle) image.Image
+	}
+
+	// img is an Image interface. This checks if the underlying value has a
+	// method called SubImage. If it does, then we can use SubImage to crop the
+	// image.
+	subImg, ok := img.(subImager)
+	if !ok {
+		return nil, fmt.Errorf("image does not support cropping")
+	}
+
+	return subImg.SubImage(crop), nil
+}
--- models/image.go
+++ models/image.go
@@ -8,9 +8,9 @@ import (
 )
 
 type Image struct {
-	X    int64 `json:"x"`
-	Y    int64 `json:"y"`
-	Zoom int64 `json:"zoom"`
+	X    int `json:"x"`
+	Y    int `json:"y"`
+	Zoom int `json:"zoom"`
 
 	ImagePath string `json:"image_path"`
 	// TODO: Add the image itself (a file) max/min x,y,zoom and improve parser/constructor
@@ -23,28 +23,8 @@ const maxY = 3
 
 func New(image, x, y, zoom string) (Image, error) {
 
-	// parse X
-	intX, err := strconv.ParseInt(x, 10, 64)
-	if err != nil {
-		return Image{}, err
-	}
-
-	if intX < minX && intX > maxX {
-		return Image{}, fmt.Errorf("x is not in [%d..%d]", minX, maxX)
-	}
-
-	// parse Y
-	intY, err := strconv.ParseInt(y, 10, 64)
-	if err != nil {
-		return Image{}, err
-	}
-
-	if intY < minY && intY > maxY {
-		return Image{}, fmt.Errorf("y is not in [%d..%d]", minY, maxY)
-	}
-
 	// parse Zoom
-	intZoom, err := strconv.ParseInt(zoom, 10, 64)
+	intZoom, err := strconv.Atoi(zoom)
 	if err != nil {
 		return Image{}, err
 	}
@@ -58,6 +38,34 @@ func New(image, x, y, zoom string) (Image, error) {
 		}
 	default:
 		return Image{}, fmt.Errorf("zoom is not in [1, 2, 4]")
+	}
+
+	// parse X
+	intX, err := strconv.Atoi(x)
+	if err != nil {
+		return Image{}, err
+	}
+
+	if intX < minX && intX > maxX {
+		return Image{}, fmt.Errorf("x is not in [%d..%d]", minX, maxX)
+	}
+
+	if intX >= intZoom {
+		return Image{}, fmt.Errorf("x: %d can not be greater than zoom: %d", intX, intZoom)
+	}
+
+	// parse Y
+	intY, err := strconv.Atoi(y)
+	if err != nil {
+		return Image{}, err
+	}
+
+	if intY < minY && intY > maxY {
+		return Image{}, fmt.Errorf("y is not in [%d..%d]", minY, maxY)
+	}
+
+	if intY >= intZoom {
+		return Image{}, fmt.Errorf("y: %d can not be greater than zoom: %d", intY, intZoom)
 	}
 
 	// check if image exists
